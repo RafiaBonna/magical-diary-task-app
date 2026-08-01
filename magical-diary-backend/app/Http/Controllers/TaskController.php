72dc72c21a90ec40b70f45c6626ee621deee7360
RafiaBonna <rafiaakterbonna@gmail.com>
2026-08-01 23:50:25 +0600

    controller

--- magical-diary-backend/app/Http/Controllers/TaskController.php
+++ magical-diary-backend/app/Http/Controllers/TaskController.php
@@ -41,7 +41,7 @@ class TaskController extends Controller
         return response()->json($task, 200);
     }
 
-    // ৪. ডায়েরির কো To destory any wish of diary
+    // ৪. ডায়েরির To destory any wish of diary
     public function destroy($id)
     {
         $task = Task::findOrFail($id);
