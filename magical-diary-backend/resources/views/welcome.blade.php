508fddb99f91d1298258917a94f0f50436857ba1
RafiaBonna <rafiaakterbonna@gmail.com>
2026-08-15 23:01:09 +0600

    update

--- magical-diary-backend/resources/views/welcome.blade.php
+++ magical-diary-backend/resources/views/welcome.blade.php
@@ -273,6 +273,6 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
-        1234567890123456
+        12345678901234567
     </body>
 </html>
